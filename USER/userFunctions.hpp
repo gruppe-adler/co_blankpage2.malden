eb1e763cf97c8177d3d9ae907e72c6386c00ec66
nomisum <nomisum@gmail.com>
2024-10-13 11:17:59 +0200

    add base

--- USER/userFunctions.hpp
+++ USER/userFunctions.hpp
@@ -6,5 +6,6 @@
 #include "..\PV_submarine\cfgFunctions.hpp"
 #include "grad_zeus\cfgFunctions.hpp"
 #include "grad_grinder\cfgFunctions.hpp"
+#include "grad_grasscutter\cfgFunctions.hpp"
 #include "functions\objective_2\cfgFunctions.hpp"
 #include "functions\loadout\cfgFunctions.hpp"
